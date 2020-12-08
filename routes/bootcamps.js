63bef27c64c7197e9d65bb816ec09e7185b8ad4a
Miroslav Vacha <miroslavvacha@Miroslavs-MacBook-Pro.local>
2020-12-08 19:46:05 +0100

    new version of controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,36 +1,20 @@
 const express = require("express");
 
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/ctrl-bootcamps");
+
 const router = express.Router();
 
-// get all listings
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, message: "Show all listings" });
-});
-
-// get single listing details
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Get bootcamp ${req.params.id} details` });
-});
-
-// create new listing
-router.post("/", (req, res) => {
-  res.status(200).json({ success: true, message: "Create new bootcamp" });
-});
-
-// update a listing
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: `Update bootcamp ${req.params.id}` });
-});
-
-// delete listing
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: "Delete single listing bootcamp" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
